945c041079b9cea67bbf021ed4cd016503ca5315
Tjepp <thor.jeppesen@lego.com>
2022-06-13 13:45:40 +0200

    css vars for star colors

--- boilerplate/src/components/rating/index.ts
+++ boilerplate/src/components/rating/index.ts
@@ -1,11 +1,14 @@
 import { html, LitElement, css, svg } from "lit";
-import { customElement, property, state } from "lit/decorators.js";
+import { customElement, state } from "lit/decorators.js";
 import { range } from "lit/directives/range.js";
 import { map } from "lit/directives/map.js";
 
-const star = svg`<path d="M49.9 18.1c-.6-1.9-2.8-2.2-2.8-2.2l-13.1-2L28 2s-1-2-3-2-3 2-3 2l-5.9 11.9-13.1 2s-2.2.3-2.8 2.2c-.6 1.9 1 3.5 1 3.5l9.6 9.4-2.5 13s-.4 2.2 1.2 3.4c1.6 1.2 3.6.2 3.6.2L25 41.8l12.1 5.9s2 1 3.6-.2 1.2-3.4 1.2-3.4l-2.5-13 9.6-9.4c-.1-.1 1.5-1.7.9-3.6z"/>`;
+const star = svg`<path d="M4.07512 22.3085L6.21423 14.999L0.476607 10.1393C0.269663 9.96838 0.120233 9.73794 0.0485476 9.4792C-0.0231375 9.22047 -0.0136153 8.94595 0.0758251 8.69282C0.165265 8.43968 0.330304 8.22015 0.548596 8.06394C0.766889 7.90774 1.02789 7.82241 1.29627 7.8195H8.29337L10.7823 0.859942C10.8692 0.60817 11.0324 0.394579 11.2492 0.239984C11.466 0.0853893 11.7256 0 11.9918 0C12.2581 0 12.5177 0.0853893 12.7345 0.239984C12.9513 0.394579 13.1145 0.60817 13.2013 0.859942L15.7103 7.8695H22.7074C22.977 7.87512 23.2384 7.96323 23.4564 8.12197C23.6744 8.28072 23.8386 8.50247 23.9268 8.75737C24.015 9.01227 24.023 9.28809 23.9498 9.54768C23.8765 9.80728 23.7255 10.0382 23.5171 10.2093L17.6195 15.049L19.9086 22.2385C20.0126 22.4941 20.0319 22.7711 19.9638 23.0385C19.8956 23.3058 19.7436 23.5439 19.53 23.7184C19.3164 23.8929 19.0524 23.985 18.7769 23.9984C18.5014 24.0118 18.2289 23.9414 17.9994 23.7884L12.0148 19.7187L6.00432 23.7884C5.78082 23.9337 5.51778 24.0088 5.2514 23.9984C4.98503 23.988 4.72841 23.8907 4.51687 23.7284C4.30534 23.5661 4.14927 23.3431 4.07016 23.0885C3.99105 22.8338 3.99278 22.5621 4.07512 22.3085Z" />`;
+
 const defaultActive = css`#ffd500`;
 const defaultInactive = css`#cacaca`;
+const defaultOutline = css`#006db7`;
+
 @customElement(`case-rating`)
 export class CaseRating extends LitElement {
   @state()
@@ -19,20 +22,20 @@ export class CaseRating extends LitElement {
 
     button {
       all: unset;
-      fill: var(--star-active, ${defaultActive});
+      fill: var(--star-color, ${defaultActive});
     }
 
     .container:hover > button {
-      fill: ${defaultActive};
+      fill: var(--custom-star-active, ${defaultActive});
     }
 
     .container > button:hover ~ button {
-      fill: ${defaultInactive};
+      fill: var(--custom-star-inactive, ${defaultInactive});
     }
 
     button:focus-visible {
-      outline: 1px solid #006db7;
-      box-shadow: 0 0 12px #006db7;
+      outline: 1px solid ${defaultOutline};
+      box-shadow: 0 0 12px ${defaultOutline};
     }
 
     .visually-hidden {
@@ -55,13 +58,13 @@ export class CaseRating extends LitElement {
         ${map(range(1, 6), (starNumber) => {
           return html`<button
             @click=${() => this._onClick(starNumber)}
-            style="--star-active: ${this.rating >= starNumber
-              ? defaultActive
-              : defaultInactive}"
+            style="--star-color: ${this.rating >= starNumber
+              ? `var(--custom-star-active, ${defaultActive})`
+              : `var(--custom-star-inactive, ${defaultInactive})`}"
           >
             <span class="visually-hidden"> Rate ${starNumber} out of 5 </span>
             <svg
-              viewBox="0 0 50 49"
+              viewBox="0 0 24 24"
               xmlns="http://www.w3.org/2000/svg"
               aria-hidden="true"
             >
